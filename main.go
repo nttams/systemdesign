ed59e818b844672bb7a8fb0288eae4e4e921b3f4
tamngo <ngothtam.me@gmail.com>
2024-10-23 09:14:55 +0700

    temp

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"net/http"
 )
+
+func startAnEchoServer() {
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.ListenAndServe(":3333", nil)
+}
 
 func main() {
 	fmt.Println("Start")
+	startAnEchoServer()
 	fmt.Println("End")
 }
